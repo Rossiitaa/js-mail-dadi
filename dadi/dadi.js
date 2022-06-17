2c4b871d4ac9ef745e47a635a6ba7336ee9aaff8
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-17 16:20:35 +0200

    creation file + initial comments

---
+++ dadi/dadi.js
@@ -0,0 +1,4 @@
+// creazione variabile per numero random pc
+//^ creazione variabile per numero randon utente
+    //? se il numero random pc > numero random utente, allora il pc vince
+    //? altrimenti se numero random pc < numero random utente, allora l'utente vince
