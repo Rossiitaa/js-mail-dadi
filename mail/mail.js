7d42b0a51938144b08c3d2d1307e57b2e403964d
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-17 15:49:10 +0200

    creation file + initial comments

---
+++ mail/mail.js
@@ -0,0 +1,5 @@
+//! creiamo una variabile con l'array 
+//? chiediamo all'utente di inserire la sua mail
+//# creiamo il presupposto che l'email non esista nell'array
+// controlliamo che l'email inserita dall'utente sia valida
+    // se l'email è valida gli assegnamo un valore positivo 
